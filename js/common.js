f0cfc67543ae7b43138e4692ea37350f9684e721
Виктория Захарова <zaharova181114@gmail.com>
2023-11-24 00:37:14 +0200

    edit step1

--- js/common.js
+++ js/common.js
@@ -5,7 +5,7 @@ $('.reviews-slider').slick({
 	variableWidth: true,
 	arrows: false,
 	autoplay: true,
-	autoplaySpeed: 2000,
+	autoplaySpeed: 6000,
 	responsive: [
 		{
 			breakpoint: 576,
@@ -28,7 +28,25 @@ $('.mobile-menu__close').on('click', function (e) {
 	$('.mobile-menu').fadeOut();
 });
 
+$('.btn-toggle-reviews').on('click', function (e) {
+	e.preventDefault();
+	$(this).prev('.reviews-wrapper').find('.reviews-card:hidden').slice(0, 4).slideDown();
 
+	var onBlock = $(this).prev('.reviews-wrapper').find('.reviews-card:hidden').length;
+	if (onBlock <= 0) {
+		$(this).hide();
+	}
+});
+
+$('.btn-toggle-events').on('click', function (e) {
+	e.preventDefault();
+	$('.events-card-col:hidden').slice(0, 2).slideDown();
+
+	var onBlock = $('.events-card-col:hidden').length;
+	if (onBlock <= 0) {
+		$(this).hide();
+	}
+});
 
 
 Fancybox.bind("[data-fancybox]", {
@@ -206,3 +224,29 @@ $('.up').on("click", function () {
 	return false;
 });
 
+$(function () {
+	//Сменим язык календаря на русский
+	$.datepicker.setDefaults(
+		{
+			closeText: 'Закрыть',
+			prevText: '',
+			currentText: 'Сегодня',
+			monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+				'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+			monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн',
+				'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+			dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+			dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+			dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+			weekHeader: 'Не',
+			dateFormat: 'dd.mm.yy',
+			firstDay: 1,
+			isRTL: false,
+			showMonthAfterYear: false,
+			yearSuffix: ''
+		});
+});
+
+$(function () {
+	$(".datepicker").datepicker();
+});
